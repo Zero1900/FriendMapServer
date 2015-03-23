8dcb4a50b54820c50fc075c36590912f830c1995
Zero1900 <363976524@qq.cpm>
2015-03-23 18:43:23 +0800

    First

---
+++ src/com/fm/servlet/client/UploadFile.java
@@ -0,0 +1,23 @@
+package com.fm.servlet.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fm.servlet.FMRequest;
+import com.fm.servlet.FMResponse;
+import com.fm.servlet.ServletBase;
+
+public class UploadFile extends ServletBase {
+
+	@Override
+	public int doSolve(FMRequest fmRequest, FMResponse fmResponse) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
